16c29e8be44318caf1362f9c85d7e454809990eb
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-05-09 02:17:36 +0530

    FAQ file added

---
+++ src/views/FAQ.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid class="my-0 py-3 d-none d-sm-block">
+    <v-layout wrap align-center justify-center row fill-height class="mt-10 mb-5">
+      <v-flex xs12 md10>
+        <v-expansion-panels accordion>
+          <v-expansion-panel v-for="(item,i) in faqData" :key="i">
+            <v-expansion-panel-header class="google-font">{{item.name}}</v-expansion-panel-header>
+            <v-expansion-panel-content class="google-font grey lighten-4 py-3">{{item.des}}</v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import data from "@/assets/data/faq.json";
+export default {
+  data() {
+    return {
+      faqData: data
+    };
+  }
+};
+</script>
