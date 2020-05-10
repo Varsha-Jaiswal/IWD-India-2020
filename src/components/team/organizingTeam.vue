7291c9de98a51e01eb1ec2a310f1e1d420850351
Sufail <sufail@glaubetech.com>
2020-05-10 21:50:21 +0530

    poornima-added team data sorting for web

--- src/components/team/organizingTeam.vue
+++ src/components/team/organizingTeam.vue
@@ -5,7 +5,7 @@
         sm4
         md2
         lg2
-        v-for="(item,i) in ShuffleData(TeamDetails)"
+        v-for="(item,i) in TeamData"
         :key="i"
         class="text-xs-center team-wrapper "
         style="text-align:center">
@@ -28,6 +28,18 @@ export default {
       showData: false
     };
   },
+    computed: {
+        TeamData: function() {
+            function compare(a, b) {
+                if (a.name < b.name)
+                    return -1;
+                if (a.name > b.name)
+                    return 1;
+                return 0;
+            }
+            return this.TeamDetails.slice().sort(compare);
+        }
+    },
   methods: {
     ShuffleData(TeamDetails) {
       let currentIndex = TeamDetails.length,
