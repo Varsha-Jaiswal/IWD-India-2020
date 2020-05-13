39e0f3cc022cf991ea4e4c11e088eaa4d9056cbc
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-05-13 19:38:58 +0530

    Component structure updated

---
+++ src/components/resources/ResourcesMobileView.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+    hi
+  </v-container>
+</template>
--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,5 +1,30 @@
 <template>
-    <v-content>
-        hi
-    </v-content>
+  <v-content class="pa-0">
+    <v-container fluid class="my-0 py-0 hidden-sm-and-up">
+      <v-layout wrap align-center justify-center row fill-height class="mt-0">
+        <v-flex xs12 md10>
+          <ResourcesMobileView />
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container fluid class="my-0 py-5 d-none d-sm-block">
+      <v-layout wrap align-center justify-center row fill-height class="mt-0">
+        <v-flex xs12 md10>
+          <Resources />
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
+
+<script>
+import Resources from "@/components/resources/Resources";
+import ResourcesMobileView from "@/components/resources/ResourcesMobileView";
+export default {
+  components: {
+    Resources,
+    ResourcesMobileView
+  }
+};
+</script>
