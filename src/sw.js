05a882b79806b8744d81458ced1f83ae3e7af158
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-05-19 06:06:08 +0530

    Service worker fixed

--- src/sw.js
+++ src/sw.js
@@ -1,21 +1,28 @@
 self.addEventListener('message', (e) => {
-    if (!e.data) {
-      return;
-    }
-  
-    switch (e.data) {
-      case 'skipWaiting':
-        self.skipWaiting();
-        break;
-      default:
-        // NOOP
-        break;
-    }
-  });
-  
-  workbox.clientsClaim();
-  
-  // The precaching code provided by Workbox.
-  self.__precacheManifest = [].concat(self.__precacheManifest || []);
-  workbox.precaching.suppressWarnings();
-  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting();
+      break;
+    default:
+      // NOOP
+      break;
+  }
+});
+
+// workbox.clientsClaim();
+
+// The precaching code provided by Workbox.
+// self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.suppressWarnings();
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.core.setCacheNameDetails({
+  prefix: "cache"
+});
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'IWD India 2020 App',
+    workboxPluginMode: 'InjectManifest',
+    themeColor: '#4A90E2',
+    workboxOptions: {
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+    },
+  }
 }
