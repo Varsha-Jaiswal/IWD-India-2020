9ffa6aab66d3075f9ebd1a43cd9acaf910829167
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-06-02 04:04:15 +0530

    Tweet UI implemented

--- src/views/Testimonials.vue
+++ src/views/Testimonials.vue
@@ -1,9 +1,9 @@
 <template>
   <v-content class="pa-5">
     <v-container fluid class="px-1 pa-0">
-      <v-layout row wrap align-center justify-center fill-height class="mt-5">
+      <v-layout row wrap align-center justify-center fill-height class="mt-0">
         <v-flex xs12 md10>
-          <Testimonials class="pa-0 mt-5" />
+          <Testimonials class="pa-0 mt-0" />
         </v-flex>
       </v-layout>
     </v-container>
