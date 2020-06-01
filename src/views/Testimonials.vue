f38cbc902104b198375913fe905dc4c7cbf98da0
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2020-06-02 03:34:42 +0530

    View update

---
+++ src/views/Testimonials.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-content class="pa-5">
+    <v-container fluid class="px-1 pa-0">
+      <v-layout row wrap align-center justify-center fill-height class="mt-5">
+        <v-flex xs12 md10>
+          <Testimonials class="pa-0 mt-5" />
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+import Testimonials from "@/components/testimonials/testimonials.vue";
+export default {
+  components: {
+    Testimonials
+  }
+};
+</script>
